b7ce57a40c0dce241c15df5c61d3cd6e71c45e26
4Tipy <lchavez21@jaumebalmes.net>
2022-10-25 20:10:18 +0200

    cambio en DeleteMapping ciclo

--- proyecto_demo2/src/main/java/net/jaumebalmes/aplicacion/controllers/ControladorCiclo.java
+++ proyecto_demo2/src/main/java/net/jaumebalmes/aplicacion/controllers/ControladorCiclo.java
@@ -2,6 +2,7 @@ package net.jaumebalmes.aplicacion.controllers;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,8 +27,9 @@ public class ControladorCiclo {
 	}
 	//Metodo para eliminar por Id
 	  @DeleteMapping("ciclo/delete/{id}")
-	  public void delete(@PathVariable Long id) {
-		cicloRep.deleteById(id);
+	  public ResponseEntity<?> borrarProducto(@PathVariable long id){
+		 cicloRep.deleteById(id);
+		 return ResponseEntity.noContent().build();
 	  }
 	  
 	  @GetMapping("ciclo/edit/{id}")
